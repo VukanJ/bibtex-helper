2de2d9e42d47eadc25c9b8706fd868ae38226867
Vukan Jevtic <vukan.jevtic@tu-dortmund.de>
2016-09-11 17:58:37 +0200

    Initial commit

---
+++ spec/bibtex-helper-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import BibtexHelperView from '../lib/bibtex-helper-view';
+
+describe('BibtexHelperView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
